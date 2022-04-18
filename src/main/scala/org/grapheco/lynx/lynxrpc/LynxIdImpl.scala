abe4cf0b67b091b2d4b24b1115326553136ae39a
Airzihao <airzihao@163.com>
2022-04-18 11:20:31 +0800

    Add LynxIdImpl.

---
+++ src/main/scala/org/grapheco/lynx/lynxrpc/LynxIdImpl.scala
@@ -0,0 +1,20 @@
+package org.grapheco.lynx.lynxrpc
+
+import org.grapheco.lynx.{LynxId, LynxInteger}
+
+/**
+ * @Author: Airzihao
+ * @Description:
+ * @Date: Created at 10:20 2022/4/18
+ * @Modified By:
+ */
+class LynxIdImpl(lynxIdValue: Any) extends LynxId{
+  override val value: Any = lynxIdValue
+
+  override def toLynxInteger: LynxInteger = value match {
+    case lynxInteger: LynxInteger => lynxInteger
+    case long: Long => LynxInteger(long)
+    case int: Int => LynxInteger(int)
+    case _ => throw new Exception("Unexpected LynxId Value.")
+  }
+}
